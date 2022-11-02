1cdd349c39c5e97f048afb3a9583d7a4db6bf380
jonhopper-dataengineers <jon@dataengineers.co.nz>
2022-11-03 10:46:26 +1300

    fixed up stage issue

--- macros/stages/snowflake/snowflake_create_stages_if_not_exist_statement.sql
+++ macros/stages/snowflake/snowflake_create_stages_if_not_exist_statement.sql
@@ -1,8 +1,7 @@
 {%- macro snowflake_create_stages_if_not_exist_statement(relation, sql) -%}
 
     {{ log("Creating stages " ~ relation) }}
-CREATE STAGE IF NOT EXISTS 
-
+CREATE STAGE IF NOT EXISTS {{ relation.include(database=(not temporary), schema=(not temporary)) }}
     {{ sql }}
     ;
 
