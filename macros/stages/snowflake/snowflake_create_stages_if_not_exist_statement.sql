c475da182741282bb73844ec28ad2ab2cbb160c1
jonhopper-dataengineers <jon@dataengineers.co.nz>
2022-11-03 10:38:52 +1300

    updated ability to use separate database

--- macros/stages/snowflake/snowflake_create_stages_if_not_exist_statement.sql
+++ macros/stages/snowflake/snowflake_create_stages_if_not_exist_statement.sql
@@ -1,7 +1,8 @@
 {%- macro snowflake_create_stages_if_not_exist_statement(relation, sql) -%}
 
     {{ log("Creating stages " ~ relation) }}
-CREATE STAGE IF NOT EXISTS {{ relation.include(database=(not temporary), schema=(not temporary)) }}
+CREATE STAGE IF NOT EXISTS 
+
     {{ sql }}
     ;
 
