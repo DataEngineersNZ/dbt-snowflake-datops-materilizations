bcf523bc14e8d76f27a0cf569fe282096111687c
jonhopper-dataengineers <jon@dataengineers.co.nz>
2022-11-24 16:34:31 +1300

    fixed spaces

--- macros/user_defined_functions/snowflake_create_external_user_defined_functions_statement.sql
+++ macros/user_defined_functions/snowflake_create_external_user_defined_functions_statement.sql
@@ -1,16 +1,16 @@
 {%- macro snowflake_create_external_user_defined_functions_statement(relation, is_secure, immutable, parameters, return_type, api_integration, api_uri) -%}
 
 {% if is_secure  %}
-CREATE IF NOT EXISTS SECURE EXTERNAL FUNCTION {{ relation.include(database=(not temporary), schema=(not temporary)) }}({{ parameters }})
+CREATE SECURE EXTERNAL FUNCTION IF NOT EXISTS {{ relation.include(database=(not temporary), schema=(not temporary)) }}({{ parameters }})
 {% else %}
-CREATE IF NOT EXISTS EXTERNAL FUNCTION {{ relation.include(database=(not temporary), schema=(not temporary)) }}({{ parameters }})
+CREATE  EXTERNAL FUNCTION IF NOT EXISTS {{ relation.include(database=(not temporary), schema=(not temporary)) }}({{ parameters }})
 {% endif %}
 RETURNS {{ return_type }}
-{%- if immutable -%}
+{% if immutable %}
     IMMUTABLE
-{%- else -%}
+{% else -%}
    VOLATILE
-{%- endif -%}
-API_INTEGRATION {{ api_integration }}
-AS {{ api_uri }};
+{% endif %}
+API_INTEGRATION = {{ api_integration }}
+AS '{{ api_uri }}';
 {%- endmacro -%}
