207aafb93030865995ed37ae45cfe0d649538ef5
jonhopper-dataengineers <jon@dataengineers.co.nz>
2022-12-07 11:38:55 +1300

    fixed up payload variable allowed for metadata columns to be defined in yml

--- macros/source_tables/snowflake/snowflake_get_copy_sql.sql
+++ macros/source_tables/snowflake/snowflake_get_copy_sql.sql
@@ -5,33 +5,35 @@
     {%- set external = source_node.external -%}
     {%- set is_csv = dbt_dataengineers_materilizations.is_csv(external.file_format, relation.database) %}
     {%- set copy_options = external.snowpipe.get('copy_options', none) -%}
-   
     {%- if explicit_transaction -%} begin; {%- endif %}
     
-    copy into {{ relation.include(database=(not temporary), schema=(not temporary)) }}
-    from ( 
-        select
+    COPY INTO {{ relation.include(database=(not temporary), schema=(not temporary)) }}
+    FROM ( 
+        SELECT
         {% if columns|length == 0 %}
-            $1::variant as value,
+            $1::VARIANT AS payload,
         {% else -%}
         {%- for column in columns -%}
+            {%- if column.name not in ["metadata_filename", "metadata_file_row_number", "import_timestamp"] -%}
             {%- set col_expression -%}
                 {%- if is_csv -%}nullif(${{loop.index}},''){# special case: get columns by ordinal position #}
+                {%- elif column.name|lower == "payload" -%}$1
                 {%- else -%}nullif($1:{{column.name}},''){# standard behavior: get columns by name #}
                 {%- endif -%}
             {%- endset -%}
-            {{col_expression}}::{{column.data_type}} as {{column.name}},
+            {{col_expression}}::{{column.data_type}} AS {{column.name}},
+            {% endif -%}    
         {% endfor -%}
         {% endif %}
-            metadata$filename::varchar as metadata_filename,
-            metadata$file_row_number::bigint as metadata_file_row_number,
-            current_timestamp::timestamp as _dbt_copied_at
-        from {{external.location | replace("@", "@" ~ relation.database ~ ".")}} {# stage #}
+        metadata$filename::VARCHAR AS metadata_filename,
+        metadata$file_row_number::BIGINT AS metadata_file_row_number,
+        current_timestamp::timestamp_ltz AS import_timestamp
+        FROM {{external.location | replace("@", "@" ~ relation.database ~ ".")}} {# stage #}
     )
-    file_format = {{relation.database ~ "." ~ external.file_format}}
+    FILE_FORMAT = {{relation.database ~ "." ~ external.file_format}}
     {% if external.pattern -%} pattern = '{{external.pattern}}' {%- endif %}
     {% if copy_options %} {{copy_options}} {% endif %};
     
-    {% if explicit_transaction -%} commit; {%- endif -%}
+    {% if explicit_transaction -%} COMMIT; {%- endif -%}
 
 {% endmacro %}
