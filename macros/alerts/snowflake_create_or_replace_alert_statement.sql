d861a05f28aa8b2b8a1f59b426da5755c630e011
jonhopper-dataengineers <jon@dataengineers.co.nz>
2023-02-10 17:08:20 +1300

    fixed up alert creation format

--- macros/alerts/snowflake_create_or_replace_alert_statement.sql
+++ macros/alerts/snowflake_create_or_replace_alert_statement.sql
@@ -1,15 +1,15 @@
 {%- macro snowflake_create_or_replace_alert_statement(relation, warehouse, schedule, severity, action, sql) -%}
 
 {{ log("Creating Alert " ~ relation) }}   
-CREATE OR REPLACE ALERT {{ relation.include(database=(not temporary), schema=(not temporary)) }}({{ parameters }})
+CREATE OR REPLACE ALERT {{ relation.include(database=(not temporary), schema=(not temporary)) }}
     WAREHOUSE {{ warehouse }}
     SCHEDULE  {{ schedule }}
     IF( EXISTS(
         {{ sql }}
     ))
     THEN
-        {% if action == "snowwatch" %}
-            CALL snowwatch.sp_sendalert('{{ relation.identifier }}', '{{ severity }}', last_query_id());
+        {%- if action == "snowwatch" -%}
+            CALL snowwatch.sp_sendalert('{{ relation.identifier }}', '{{ severity }}', last_query_id())
         {%- else -%}
             {{ action }}
         {%- endif -%}
