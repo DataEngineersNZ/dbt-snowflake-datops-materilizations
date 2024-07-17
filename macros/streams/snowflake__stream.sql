ab46505495ac261a33a9af8c84da4d85bd2da1bd
jonhopper-dataengineers <jon@dataengineers.co.nz>
2024-07-17 19:59:56 +1200

    added capability for source_database_prefix

--- macros/streams/snowflake__stream.sql
+++ macros/streams/snowflake__stream.sql
@@ -9,8 +9,13 @@
   {%- set source_model = config.get('source_model') -%}
   {%- set source_schema = config.get('source_schema', default=schema) -%}
   {%- set source_database = config.get('source_database', default=database) -%}
+  {%- set source_database_prefix = config.get('source_database_prefix', default=none) -%}
   {%- set source_type = config.get('source_type', default='internal') -%}
 
+  {% if source_database_prefix is not none %}
+    {% set source_database_var = source_database_prefix ~ "_" ~  target.name|replace('-', '_') %}
+    {% set source_database = var(source_database_var|replace("__", "_")|lower, database)   %}
+  {% endif %}
   {% set target_relation = this %}
   {% set source_relation = adapter.get_relation(identifier=source_model, schema=source_schema, database=source_database) %}
 
