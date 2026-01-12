beca222c7ff6455f40e72657f607235ee8a0b3fa
Anton Stechman <Anton.Stechman@alliance.co.nz>
2026-01-13 10:18:28 +1300

    updated default statement for consistency

--- macros/file_formats/snowflake/snowflake__create_fileformat_statement.sql
+++ macros/file_formats/snowflake/snowflake__create_fileformat_statement.sql
@@ -1,4 +1,4 @@
-{%- macro snowflake_create_fileformat_statement(relation, sql, create_or_replace="CREATE FILE FORMAT IF NOT EXISTS") -%}
+{%- macro snowflake_create_fileformat_statement(relation, sql, create_or_replace="create or replace file format") -%}
 
     {{ log("Creating fileformat " ~ relation) }}
 {{ create_or_replace }} {{ relation.include(database=(not temporary), schema=(not temporary)) }}
