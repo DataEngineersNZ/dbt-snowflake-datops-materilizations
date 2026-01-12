8e1373a282090659eada2f15ac4e6c35cd2e53ce
Anton Stechman <Anton.Stechman@alliance.co.nz>
2026-01-13 09:55:47 +1300

    patch for snowflake_create_fileformat_statement. setting param 'create_or_replace' as an optional with a default of "CREATE IF NOT EXISTS".
    This is needed to patch an error:
      'str object' has no attribute 'include'
    
      > in macro snowflake__get_file_format_build_plan (macros/file_formats/snowflake/snowflake__get_file_format_build_plan.sql)
      > called by macro get_file_format_build_plan (macros/file_formats/get_file_format_build_plan.sql)
      > called by macro stage_file_format_plans (macros/file_formats/stage_file_formats.sql)
      > called by macro stage_file_formats (macros/file_formats/stage_file_formats.sql)

--- macros/file_formats/snowflake/snowflake__create_fileformat_statement.sql
+++ macros/file_formats/snowflake/snowflake__create_fileformat_statement.sql
@@ -1,4 +1,4 @@
-{%- macro snowflake_create_fileformat_statement(create_or_replace, relation, sql) -%}
+{%- macro snowflake_create_fileformat_statement(relation, sql, create_or_replace="CREATE IF NOT EXISTS") -%}
 
     {{ log("Creating fileformat " ~ relation) }}
 {{ create_or_replace }} {{ relation.include(database=(not temporary), schema=(not temporary)) }}
