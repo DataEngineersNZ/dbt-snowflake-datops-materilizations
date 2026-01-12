9571b392983f6185218794b9ea209da1b8c72a1b
Anton Stechman <Anton.Stechman@alliance.co.nz>
2026-01-13 10:10:28 +1300

    sourcery-ai suggested patch, initial default violates snowflake DDL and would produce an invalid command

--- macros/file_formats/snowflake/snowflake__create_fileformat_statement.sql
+++ macros/file_formats/snowflake/snowflake__create_fileformat_statement.sql
@@ -1,4 +1,4 @@
-{%- macro snowflake_create_fileformat_statement(relation, sql, create_or_replace="CREATE IF NOT EXISTS") -%}
+{%- macro snowflake_create_fileformat_statement(relation, sql, create_or_replace="CREATE FILE FORMAT IF NOT EXISTS") -%}
 
     {{ log("Creating fileformat " ~ relation) }}
 {{ create_or_replace }} {{ relation.include(database=(not temporary), schema=(not temporary)) }}
